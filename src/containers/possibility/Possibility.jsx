680809f84f4f277d471579cbd9329e53201cb8d9
akashagl <martkrupik@gmail.com>
2022-03-19 18:46:05 +0000

    commit

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -8,10 +8,10 @@ const Possibility = () => (
       <img src={possibilityImage} alt="possibility" />
     </div>
     <div className="gpt3__possibility-content">
-      <h4>Request Early Access to Get Started</h4>
+      <h4>Early Access Soon</h4>
       <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
       <p>Akasha builds technologies that help people connect, find communities, and grow businesses. It will let you share immersive experiences with other people even when you can’t be together — and do things together you couldn’t do in the physical world. It’s the next evolution in a long line of social technologies. be part of us.</p>
-      <h4>Request Early Access to Get Started</h4>
+      <h4>Education & Sustainability is key.</h4>
     </div>
   </div>
 );
