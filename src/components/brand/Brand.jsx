5ef58827fcf1c61ba38f2c810ee1f610ed881470
akashagl <martkrupik@gmail.com>
2022-03-19 19:51:13 +0000

    commit

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { google, slack, atlassian, dropbox, shopify } from './imports';
+import { google, dropbox, shopify } from './imports';
 import './brand.css';
 
 const Brand = () => (
