8da9bdc7b882579ba13ad283650fa2e6ede9fdd5
akashagl <martkrupik@gmail.com>
2022-03-19 18:51:37 +0000

    commit

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -5,11 +5,11 @@ import './features.css';
 const featuresData = [
   {
     title: 'Create.',
-    text: 'Create your own personlized world, a world you feel the most comfortable and proud at and share it with others.',
+    text: 'Create your own personalized world, a world you feel the most comfortable and proud at and share it with others.',
   },
   {
     title: 'Explore.',
-    text: 'Explore your freinds worlds and see the life from their perspectives.',
+    text: 'Explore your friends worlds and see the life from their perspectives.',
   },
   {
     title: 'Trade.',
@@ -25,7 +25,7 @@ const Features = () => (
   <div className="gpt3__features section__padding" id="features">
     <div className="gpt3__features-heading">
       <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
-      <p>Request Early Access to Get Started</p>
+      <p>Opening Soon</p>
     </div>
     <div className="gpt3__features-container">
       {featuresData.map((item, index) => (
