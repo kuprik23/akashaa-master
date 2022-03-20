7a63773787e1cb53851772b3b9e9b2bd880acfb9
akashagl <martkrupik@gmail.com>
2022-03-20 19:05:51 +0000

    COMMIT

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -8,10 +8,11 @@ const Footer = () => (
       <h1 className="gradient__text">Do you want to step in to the future before others</h1>
     </div>
 
-<p></p>
+<p>
     <div className="gpt3__footer-btn">
     <a target="_blank" href="https://akasha.gl/akasha2"> <button id="bt" >Learn More</button> </a>
     </div>
+    </p>
 
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
