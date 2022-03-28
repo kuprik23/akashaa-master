7e8c8ca31903f54ce584ffe6f1ecf20072a9f183
akashagl <martkrupik@gmail.com>
2022-03-28 23:09:42 +0100

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             <a href="#wgpt3">What is Akasha?</a>
           </p>
           <p>
-            <a href="#possibility">Mint</a>
+          <a target="_blank" href="https://akasha3.netlify.app"> <button id="bt" >Mint</button> </a>
           </p>
           <p>
             <a
