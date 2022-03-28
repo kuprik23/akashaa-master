11883f03e35f1e8eb5ccfdb9e3ce013e8a23b3f7
akashagl <martkrupik@gmail.com>
2022-03-28 23:25:26 +0100

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             <a href="#wgpt3">What is Akasha?</a>
           </p>
           <p>
-          <a target="_blank" href="https://akasha-git-master-ridaen-nasry.vercel.app/"> <button id="bt" >Mint</button> </a>
+          <a href="https://akasha-git-master-ridaen-nasry.vercel.app/"> <button id="bt" >Mint</button> </a>
           </p>
           <p>
             <a
@@ -63,7 +63,7 @@ const Navbar = () => {
                 <a href="#wgpt3">What is Akasha</a>
               </p>
               <p>
-              <a href="https://akasha-git-master-ridaen-nasry.vercel.app/"> <button id="bt" >Mint</button> </a>
+              <a target="_blank" href="https://akasha-git-master-ridaen-nasry.vercel.app/"> <button id="bt" >Mint</button> </a>
               </p>
               <p>
                 <a href="#features">White Paper</a>
