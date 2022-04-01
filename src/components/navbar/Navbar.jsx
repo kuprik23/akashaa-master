7609c6ba216d9c4fe4109eb5f102c59dac6c780a
akashagl <martkrupik@gmail.com>
2022-04-01 22:27:18 +0100

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,10 +2,79 @@ import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../logo.png';
 import './navbar.css';
+import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+  { Button, Paper, Stack, Typography } from "@mui/material";
 
+
+const MetaConnect = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", accountsChanged);
+      window.ethereum.on("chainChanged", chainChanged);
+    }
+  }, []);
+
+  const connectHandler = async () => {
+    if (window.ethereum) {
+      try {
+        const res = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        await accountChange(res[0]);
+      } catch (err) {
+        console.error(err);
+        setErrorMessage("There was a problem connecting to MetaMask");
+      }
+    } else {
+      setErrorMessage("Install MetaMask");
+    }
+  };
+
+  const accountsChanged = async (newAccount) => {
+    setAccount(newAccount);
+    try {
+      const balance = await window.ethereum.request({
+        method: "eth_getBalance",
+        params: [newAccount.toString(), "latest"],
+      });
+      setBalance(ethers.utils.formatEther(balance));
+    } catch (err) {
+      console.error(err);
+      setErrorMessage("There was a problem connecting to MetaMask");
+    }
+  };
+
+  const chainChanged = () => {
+    setErrorMessage(null);
+    setAccount(null);
+    setBalance(null);
+  };
+
+  return (
+    <Paper elevation={3} sx={{ p: 3 }}>
+      <Stack spacing={2}>
+        <Typography variant="h6"> Account: {account} </Typography>
+        <Typography variant="h6">
+          Balance: {balance} {balance ? "ETH" : null}
+        </Typography>
+        <Button onClick={connectHandler}>Connect Account</Button>
+        {errorMessage ? (
+          <Typography variant="body1" color="red">
+            Error: {errorMessage}
+          </Typography>
+        ) : null}
+      </Stack>
+    </Paper>
+  );
+};
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -37,7 +106,7 @@ const Navbar = () => {
         </div>
       </div>
       <div className="gpt3__navbar-sign">
-        <button type="button">Connect Wallet</button>
+        <button type="button">Connect Wallet</button>import 
       </div>
       <div className="gpt3__navbar-menu">
         {toggleMenu ? (
